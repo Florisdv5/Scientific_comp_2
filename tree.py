b07d79f086705b4bbb9cf21c408d02afbd065096
Peer96 <roman_peerboom@hotmail.com>
2021-03-01 16:41:52 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -3,13 +3,11 @@ from SOR_funcs import find_candidates, select_value, calculate_SOR
 import matplotlib.pyplot as plt
 
 def one_step(old_grid, omega, eta):
-    new_grid = np.zeros(shape=(N, N))
     probs = np.zeros(N*N)
     probs_total = 0
+    new_grid = old_grid
     for y in range(len(old_grid)):
         for x in range(len(old_grid[0])):
-            if old_grid[y][x] == 1:
-                new_grid[y][x] = 1
             if find_candidates(old_grid, y, x) == True:
                 prob = calculate_SOR(old_grid, new_grid, y, x, omega) ** eta
                 probs[(y*N)+x] = prob
@@ -20,12 +18,13 @@ def one_step(old_grid, omega, eta):
     new_grid[i//N, i%N] = 1
     return new_grid
 
-N = 20
+N = 100
 matrix = np.zeros(shape=(N, N))
 matrix[N - 1][np.random.randint(1, N - 1)] = 1
-omega = 0.5
-eta = 1
-for step in range(100):
+omega = 1.8
+eta = 3
+for step in range(600):
     matrix = one_step(matrix, omega, eta)
-    plt.matshow(grid)
-    plt.show()
+
+plt.matshow(matrix)
+plt.show()
