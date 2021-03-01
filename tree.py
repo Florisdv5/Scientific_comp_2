c9a6063982beb9a648eb3f711f20650136138379
Floris de Vries <54944883+Florisdv5@users.noreply.github.com>
2021-03-01 21:30:48 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,36 +1,37 @@
 import numpy as np
-from SOR_funcs import find_candidates, select_value, calculate_SOR
+from SOR_funcs import update_SOR, initialise_grid, find_neighbours, concentration_neighbours
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from DLA_constants import n_size, steps, eta
 
 
-def one_step(old_grid, omega, eta):
-    probs = np.zeros(N*N) # List of 10000 zero's
-    probs_total = 0
-    new_grid = old_grid[:]
-    for y in range(len(old_grid)):
-        for x in range(len(old_grid[0])):
-            if find_candidates(old_grid, y, x) == True:
-                prob = calculate_SOR(old_grid, new_grid, y, x, omega) ** eta
-                probs[(y*N)+x] = prob
-                probs_total += prob
-    for x in range(len(probs)):
-        probs[x] = probs[x] / probs_total
-    i = select_value(probs)
-    new_grid[i//N, i%N] = 1
-    return new_grid
+def one_step(matrix_structure, matrix_concentration):
+    matrix_concentration = update_SOR(matrix_structure, matrix_concentration)
+    neighbours = find_neighbours(matrix_structure)
+    neighbours_conc = concentration_neighbours(neighbours, matrix_concentration)
+
+    sum_candidates_conc = 0
+    for i in range(len(neighbours_conc)):
+        sum_candidates_conc += neighbours_conc[i] ** eta
+    probabilities = []
+    for i in range(len(neighbours_conc)):
+        probabilities.append(neighbours_conc[i] ** eta / sum_candidates)
+
+    choice = np.random.choice(len(neighbours), p=probabilities)
+    added_neighbour = neighbours[choice]
+    matrix_structure[added_neighbour[0]][added_neighbour[1]] = 1
+    return matrix_structure, matrix_concentration
 
 
-N = 100
-matrix_structure = np.zeros(shape=(N, N))
-matrix_concentration = np.zeros(shape=(N, N))
-matrix_structure[0][int(N/2)] = 1
-omega = 1.8
-eta = 3
-# matrix = one_step(matrix, omega, eta)
-for step in tqdm(range(600)):
-    matrix = one_step(matrix_structure, omega, eta)
+y_values = np.linspace(0, 1, num=n_size + 1)
+matrix_structure = np.zeros(shape=(n_size, n_size))
+matrix_structure[0][int(n_size / 2)] = 1
+matrix_concentration = initialise_grid()
 
-y_values = np.linspace(0, 1, num=N+1)
+for step in tqdm(range(steps)):
+    matrix_structure, matrix_concentration = one_step(matrix_structure, matrix_concentration)
+
+y_values = np.linspace(0, 1, num=n_size + 1)
 plt.pcolormesh(y_values, y_values, matrix_structure)
+plt.savefig("Pictures/DLA_eta_{}".format(eta))
 plt.show()
