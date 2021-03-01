b8a159b8e849b11a10c506e91e0f3307834a43e3
Floris de Vries <54944883+Florisdv5@users.noreply.github.com>
2021-03-01 19:49:11 +0100

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,11 +1,13 @@
 import numpy as np
 from SOR_funcs import find_candidates, select_value, calculate_SOR
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 
 def one_step(old_grid, omega, eta):
-    probs = np.zeros(N*N)
+    probs = np.zeros(N*N) # List of 10000 zero's
     probs_total = 0
-    new_grid = old_grid
+    new_grid = old_grid[:]
     for y in range(len(old_grid)):
         for x in range(len(old_grid[0])):
             if find_candidates(old_grid, y, x) == True:
@@ -18,13 +20,17 @@ def one_step(old_grid, omega, eta):
     new_grid[i//N, i%N] = 1
     return new_grid
 
+
 N = 100
-matrix = np.zeros(shape=(N, N))
-matrix[N - 1][np.random.randint(1, N - 1)] = 1
+matrix_structure = np.zeros(shape=(N, N))
+matrix_concentration = np.zeros(shape=(N, N))
+matrix_structure[0][int(N/2)] = 1
 omega = 1.8
 eta = 3
-for step in range(600):
-    matrix = one_step(matrix, omega, eta)
+# matrix = one_step(matrix, omega, eta)
+for step in tqdm(range(600)):
+    matrix = one_step(matrix_structure, omega, eta)
 
-plt.matshow(matrix)
+y_values = np.linspace(0, 1, num=N+1)
+plt.pcolormesh(y_values, y_values, matrix_structure)
 plt.show()
