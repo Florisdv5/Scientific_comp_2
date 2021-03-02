e6cbc7851c4f4dca41a14d09bcb55a57ba7f4baa
Floris de Vries <54944883+Florisdv5@users.noreply.github.com>
2021-03-02 13:18:13 +0100

    Update DLA_constants.py

--- DLA_constants.py
+++ DLA_constants.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-steps = 600
+steps = 500
 n_size = 100
 prob_s = 1
-prob_s_list = np.linspace(0.1, 0.9, 9)
+# prob_s_list = np.linspace(0.1, 0.9, 9)
+prob_s_list = [0.001, 0.01, 0.1, 1]
 omega = 1
-eta = 2
+eta = 1
+eta_list = np.linspace(0,2, 9)
+
