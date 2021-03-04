320dc79b70c5b24b6f9121bbc8feec2543b01119
Floris de Vries <54944883+Florisdv5@users.noreply.github.com>
2021-03-04 15:20:41 +0100

    Update Gray_Scott.py

--- Gray_Scott.py
+++ Gray_Scott.py
@@ -30,30 +30,8 @@ y_values = np.linspace(0, 1, num=n_size + 1)
 
 # Default plot with noise
 
-fig = plt.figure(figsize=(9, 6))
-axes = fig.subplots(2, 2, sharex=True, sharey=True)
-fig.add_subplot(111, frameon=False)
-plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-plt.grid(False)
-plt.xlabel("X-value", size=15)
-plt.ylabel("Y-value", size=15)
-axes_num = 0
-for row in axes:
-    for col in row:
-        matrix_conc_u = initialise_conc("U", noise=True)
-        matrix_conc_v = initialise_conc("V", noise=True)
-        for step in tqdm(range(steps)):
-            matrix_conc_u, matrix_conc_v = update_conc(matrix_conc_u, matrix_conc_v, f_u, k)
-        axes_num += 1
-        col.pcolormesh(y_values, y_values, matrix_conc_v)
-        # col.set_title('f = {}, k = {}'.format(round(f_u, 3), round(k, 3)))
-plt.savefig("Pictures/GS_list_noise_default_9_noise_0.5.png")
-plt.show()
-
-# Plot for different for different f & k
-
 # fig = plt.figure(figsize=(9, 6))
-# axes = fig.subplots(3, 3, sharex=True, sharey=True)
+# axes = fig.subplots(2, 2, sharex=True, sharey=True)
 # fig.add_subplot(111, frameon=False)
 # plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
 # plt.grid(False)
@@ -62,17 +40,39 @@ plt.show()
 # axes_num = 0
 # for row in axes:
 #     for col in row:
-#         f_u = f_u_list[axes_num]
-#         k = k_list[axes_num]
-#         matrix_conc_u = initialise_conc("U", noise=False)
-#         matrix_conc_v = initialise_conc("V", noise=False)
+#         matrix_conc_u = initialise_conc("U", noise=True)
+#         matrix_conc_v = initialise_conc("V", noise=True)
 #         for step in tqdm(range(steps)):
 #             matrix_conc_u, matrix_conc_v = update_conc(matrix_conc_u, matrix_conc_v, f_u, k)
 #         axes_num += 1
 #         col.pcolormesh(y_values, y_values, matrix_conc_v)
-#         col.set_title('f = {}, k = {}'.format(round(f_u, 3), round(k, 3)))
-# plt.savefig("Pictures/GS_list_9")
+#         # col.set_title('f = {}, k = {}'.format(round(f_u, 3), round(k, 3)))
+# plt.savefig("Pictures/GS_list_noise_default_9_noise_0.5.png")
 # plt.show()
+
+# Plot for different for different f & k
+
+fig = plt.figure(figsize=(9, 6))
+axes = fig.subplots(3, 3, sharex=True, sharey=True)
+fig.add_subplot(111, frameon=False)
+plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+plt.grid(False)
+plt.xlabel("X-value", size=15)
+plt.ylabel("Y-value", size=15)
+axes_num = 0
+for row in axes:
+    for col in row:
+        f_u = f_u_list[axes_num]
+        k = k_list[axes_num]
+        matrix_conc_u = initialise_conc("U", noise=False)
+        matrix_conc_v = initialise_conc("V", noise=False)
+        for step in tqdm(range(steps)):
+            matrix_conc_u, matrix_conc_v = update_conc(matrix_conc_u, matrix_conc_v, f_u, k)
+        axes_num += 1
+        col.pcolormesh(y_values, y_values, matrix_conc_v)
+        col.set_title('f = {}, k = {}'.format(round(f_u, 3), round(k, 3)))
+plt.savefig("Pictures/GS_list_9")
+plt.show()
 
 # GIF animation creator
 
